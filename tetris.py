d7c50a1027321846ecf4994a00fffcea4f0570c8
rcoh <rcoh@mit.edu>
2011-09-08 01:51:56 -0400

    random tetris stuff.

--- tetris.py
+++ tetris.py
@@ -39,8 +39,8 @@ from ddrinput import DIRECTIONS
 import pygame
 
 TIME_LIMIT = 5 * 60  #seconds
-LINES_TO_ADVANCE = 8 #num lines needed to advance to next level
-LEVEL_SPEEDS = [400,300,400,250,560,520]
+LINES_TO_ADVANCE = 5 #num lines needed to advance to next level
+LEVEL_SPEEDS = [500,440,380,320,250]
 
 MAXX = 10
 MAXY = 18
