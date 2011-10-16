8af79e3cb5e5857718474a9a94d2c71b75fe76dc
rcoh <rcoh@mit.edu>
2011-10-16 19:51:40 -0400

    gif support

--- figure_builder.py
+++ figure_builder.py
@@ -15,20 +15,17 @@ def load_dict_from_image(im, im_bounds, figure_size):
   return_dict = {}
   for x in range(result_max_x):
     for y in range(result_max_y):
-      sample_loc = get_sample_loc(im_bounds, figure_size, (x, y))
-      color_at_loc = color_deref(im.getpixel(sample_loc))
+      color_at_loc = color_deref(sample(im, get_sample_loc(im_bounds, figure_size, (x, y)), 1))
       if color_at_loc != None:
         return_dict[(x, y)] = color_at_loc
   return return_dict
 
-def load_gif(file_loc): 
-
-def get_image_frame(
 def color_deref(color):
+  return color
   if color == (0, 0, 0):
-    return 1
+    return (255,0,0)
   elif color != 0:
-    return 1
+    return (255,0,0)
   else:
     return None
 
@@ -42,6 +39,20 @@ def get_sample_loc(im_bounds, figure_size, loc):
   sample_tl_x = im_tl[0] + loc[0] * grid_x
   sample_tl_y = im_tl[1] + loc[1] * grid_y
   return (sample_tl_x + grid_x / 2, sample_tl_y + grid_y / 2)
+
+def sample(im, loc, radius):
+  colors = []
+  return im.getpixel(loc)
+  for i in range(loc[0]-radius, loc[0]+radius):
+    for j in range(loc[1]-radius, loc[1]+radius):
+      colors.append(im.getpixel((i,j))) 
+  return mean_of_colors(colors)
+
+def mean_of_colors(colors):
+  r = sum([r for r,g,b in colors]) / len(colors)
+  g = sum([g for r,g,b in colors]) / len(colors)
+  b = sum([b for r,g,b in colors]) / len(colors)
+  return (r,g,b)
 
 if __name__ == "__main__":
   print dict_from_image_file(sys.argv[1], (5, 6))
